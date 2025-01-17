2d196ff37d1911dd509caffaf1c85c4a80be1c7a
Elnur Mammadov <lnur004@gmail.com>
2025-01-17 22:57:20 +0400

    Revert "added PWA (#4)" (#5)
    This reverts commit b8d70f0f14a8b7d5c52a06038e249c2788af5076.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
-import withPWA from 'next-pwa';
+import type { NextConfig } from "next";
 
-const nextConfig = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
-})({
-  /* your existing config options */
-});
+const nextConfig: NextConfig = {
+  /* config options here */
+};
 
 export default nextConfig;
